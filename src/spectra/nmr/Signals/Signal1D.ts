5c44cb8cc0787d977777338a5a2f3c508b4200c1
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-06-22 16:02:39 +0200

    chore: fix eslint

--- src/spectra/nmr/Signals/Signal1D.ts
+++ src/spectra/nmr/Signals/Signal1D.ts
@@ -1,6 +1,6 @@
 import type { JCoupling } from '../JCoupling';
-import type { Signal } from './Signal';
 import type { Peak1D } from '../Peaks/Peak1D';
+import type { Signal } from './Signal';
 
 export interface Signal1D extends Signal {
   delta: number;
