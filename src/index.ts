1f9bee022178791498bb70cc1acba4ab2ecf802a
Luc Patiny <luc@patiny.com>
2021-09-24 07:14:16 +0200

    fix: fix build and move all files to 'todo'

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-//export { InfraredSpectroscopy } from './spectra/IRSpectroscopy/IRSpectroscopy';
-//export { PXRD } from './spectra/PXRD/PXRD';
-//export { Isotherm } from './spectra/GasAdsorptionIsotherm/GasAdsorptionIsotherm';
-//export { TGA } from './spectra/TGA/TGA';
-
-//export { Value } from './general/Value';
-//export { Instrument } from './general/Instrument';
-//export { Range } from './general/Range';
-//export { DataSource } from './general/DataSource';
-
-//export { BoilingPoint } from './physical/BoilingPoint';
-
 export { DataXY } from './core/DataXY';
 export { DoubleArray } from './core/DoubleArray';
 export { SpectrumVariable } from './core/SpectrumVariable';
 export { OneLetter } from './core/OneLetter';
+export { Value } from './core/Value';
+export { Range } from './core/Range';
