6765ac25b2830de0026ff60f62f396a833027c51
Luc Patiny <luc@patiny.com>
2021-12-07 14:57:56 +0100

    fix: add optional values in icp

--- src/analysis/icp.ts
+++ src/analysis/icp.ts
@@ -2,9 +2,9 @@ import { Value } from '../core/Value.d';
 
 export interface ICPResult {
   element: string;
-  wavelength: Value;
-  experimentalConcentration: Value;
-  dilutionFactor: number;
-  solvent: string;
-  sampleConcentration: Value;
+  wavelength?: Value;
+  experimentalConcentration?: Value;
+  dilutionFactor?: number;
+  solvent?: string;
+  sampleConcentration?: Value;
 }
