cdb9276bc3855c3218806a6a1ebd550db371dc39
Luc Patiny <luc@patiny.com>
2021-12-07 15:14:46 +0100

    fix: improve ICP dilution

--- src/analysis/icp.ts
+++ src/analysis/icp.ts
@@ -1,10 +1,14 @@
 import { Value } from '../core/Value.d';
 
+export interface ICPDilution {
+  factor?: number;
+  solvent?: string;
+}
+
 export interface ICPResult {
   element: string;
   wavelength?: Value;
   experimentalConcentration?: Value;
-  dilutionFactor?: number;
-  solvent?: string;
+  dilution?: ICPDilution;
   sampleConcentration?: Value;
 }
