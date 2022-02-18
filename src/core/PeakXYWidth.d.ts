6f2617523b9d8447b2564c8a12c0998a34353df9
Luc Patiny <luc@patiny.com>
2022-02-18 10:37:19 +0100

    chore: fix PeakXYWidth and PointXY

--- src/core/PeakXYWidth.d.ts
+++ src/core/PeakXYWidth.d.ts
@@ -1,28 +1,5 @@
-export interface PointXY {
-  a?: number;
-  b?: number;
-  c?: number;
-  d?: number;
-  e?: number;
-  f?: number;
-  g?: number;
-  h?: number;
-  i?: number;
-  j?: number;
-  k?: number;
-  l?: number;
-  m?: number;
-  n?: number;
-  o?: number;
-  p?: number;
-  q?: number;
-  r?: number;
-  s?: number;
-  t?: number;
-  u?: number;
-  v?: number;
-  w?: number;
+export interface PeakXYWidth {
   x: number;
   y: number;
-  z?: number;
+  width: number;
 }
--- src/core/PointXY.d.ts
+++ src/core/PointXY.d.ts
@@ -1,5 +1,28 @@
-export interface PeakXYWidth {
+export interface PointXY {
+  a?: number;
+  b?: number;
+  c?: number;
+  d?: number;
+  e?: number;
+  f?: number;
+  g?: number;
+  h?: number;
+  i?: number;
+  j?: number;
+  k?: number;
+  l?: number;
+  m?: number;
+  n?: number;
+  o?: number;
+  p?: number;
+  q?: number;
+  r?: number;
+  s?: number;
+  t?: number;
+  u?: number;
+  v?: number;
+  w?: number;
   x: number;
   y: number;
-  width: number;
+  z?: number;
 }
