f85ad4fd4feff38b057b03770bcad2c30298b01f
Luc Patiny <luc@patiny.com>
2022-02-18 10:53:33 +0100

    chore: remove docs from github

--- docs/buildDocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/sh
-for filename in json/*/*.json; do
-    file=${filename/json/}
-    echo $file
-    echo $filename
-    generate-schema-doc --config-file "docs/config.yml" "$filename" "docs${file/.json/.html}"
-done
