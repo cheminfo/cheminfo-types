25d9aa3fe617dc78931032c4b8e4219c0a1a3827
jobo322 <jose.bolanos@correounivalle.edu.co>
2023-06-08 09:30:48 -0500

    refactor!: rename Data2D to NmrData2D

---
+++ src/spectra/NmrData2D.ts
@@ -0,0 +1,23 @@
+export interface NmrData2DContent {
+  z: Float64Array[];
+  minZ: number;
+  maxZ: number;
+  minY: number;
+  maxY: number;
+  minX: number;
+  maxX: number;
+}
+
+export type NmrData2D = NmrData2DFid | NmrData2DFt;
+
+export interface NmrData2DFid {
+  re: NmrData2DContent;
+  im?: NmrData2DContent;
+}
+
+export interface NmrData2DFt {
+  rr: NmrData2DContent;
+  ri?: NmrData2DContent;
+  ir?: NmrData2DContent;
+  ii?: NmrData2DContent;
+}
