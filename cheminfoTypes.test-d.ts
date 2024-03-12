49557ee1b7dc64eef0ac2f0ebd8a0266e4a3757e
Michaël Zasso <targos@protonmail.com>
2024-03-12 10:50:50 +0100

    fix: allow all NumberArray in DataXY (#99)
    It's a generic enough type that we shouldn't restrict it too much.
    
    Refs: https://github.com/mljs/spectra-processing/pull/224

--- cheminfoTypes.test-d.ts
+++ cheminfoTypes.test-d.ts
@@ -1,6 +1,11 @@
 import { pino } from 'pino';
 import { expectAssignable } from 'tsd';
 
-import type { Logger } from '.';
+import type { Logger, DataXY } from '.';
 
 expectAssignable<Logger>(pino());
+
+expectAssignable<DataXY>({
+  x: new Float32Array(),
+  y: new Float32Array(),
+});
